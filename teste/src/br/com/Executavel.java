92580c56b85bca9fab662bb414e1baec7535e449
boese <boese@DESKTOP-668O27G>
2017-12-08 22:08:06 -0200

    incluindo o v2

--- teste/src/br/com/Executavel.java
+++ teste/src/br/com/Executavel.java
@@ -4,7 +4,7 @@ public class Executavel {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("teste de versionamento no git.");
+		System.out.println("teste de versionamento no git v2.");
 	}
 
 }
