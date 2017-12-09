9e15ddb01b6d5634f1e88730c6c7c381cfca51f8
boese <boese@DESKTOP-668O27G>
2017-12-08 22:14:14 -0200

    comitando o restante

--- teste/src/br/com/Executavel.java
+++ teste/src/br/com/Executavel.java
@@ -4,7 +4,7 @@ public class Executavel {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("teste de versionamento no git v2.");
+		System.out.println("teste de versionamento no git v2, só comitando uma parte.");
 	}
 
 }
